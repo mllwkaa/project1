af17bf7928fc0a5b7e3e151b1561cc2577103ee5
melika <melikaarjomandfard@gmail.com>
2024-11-09 22:25:30 +0330

    new if

--- math2.java
+++ math2.java
@@ -6,7 +6,8 @@ public class math2 {
         System.out.println("enter a number:");
         Scanner input =new Scanner(System.in);
         double  x = input.nextDouble();
-        
+        if (x=0)
+        System.out.println("0");
         if (x/2 == 0) 
             System.out.println("hi even");
          if (x/5 == 0) 
